ae1bea69de5eb03d8cb303bbbda246273ebef09f
Prateek Thapa <prateekthapa26@gmail.com>
2020-09-23 14:06:53 +0545

    add urlscheme

--- app-installed-checker.js
+++ app-installed-checker.js
@@ -2,13 +2,13 @@ import { Linking, Platform } from 'react-native';
 import { APP_LIST } from './app-list';
 import CheckPackageInstallation from './android';
 
-export const isAppInstalledAndroid = key =>
+const isAppInstalledAndroid = key =>
     new Promise((res, rej) =>
         CheckPackageInstallation
             .isPackageInstalled(key, (isInstalled) => res(isInstalled))
     )
 
-export const isAppInstalledIOS = (key) =>
+const isAppInstalledIOS = (key) =>
     new Promise((res, rej) =>
         Linking
             .canOpenURL(key.urlScheme + '://' + (key.urlParams || ''))
@@ -16,9 +16,14 @@ export const isAppInstalledIOS = (key) =>
             .catch(err => rej(err))
     )
 
-export const isAppInstalled = (keyOrPackage) =>
+const isAppInstalled = (keyOrPackage) =>
     Platform.select({
         ios: () => isAppInstalledIOS(APP_LIST[keyOrPackage] || keyOrPackage),
         android: () => isAppInstalledAndroid(APP_LIST[keyOrPackage] || keyOrPackage),
     })
 
+export {
+    isAppInstalled, 
+    isAppInstalledIOS,
+    isAppInstalledAndroid,
+}
--- app-list.js
+++ app-list.js
@@ -8,7 +8,7 @@ export const APP_LIST = {
     "facebook": createPackage("com.facebook.katana", "fb", "requests"),
     "whatsapp": createPackage("com.whatsapp", "whatsapp", "app"),
     "instagram": createPackage("com.instagram.android", "instagram",  "app"),
-    "viber": createPackage("com.viber.voip", ),
+    "viber": createPackage("com.viber.voip", "viber"),
     "facebook_messenger": createPackage("com.facebook.orca", "fb-messenger", "user-thread/{user-id}"),
     "gmail": createPackage("com.google.android.gm", "googlegmail", "co?subject=foo&body=bar"),
     "snapchat": createPackage("com.snapchat.android", "snapchat", "?u=foo"),
